935c370a1aaa292f4c5c3dda515191ec351fa0d0
Paul Harper <benekastah@gmail.com>
2024-11-05 18:52:08 -0800

    I like this better

--- app/note-trainer/page.tsx
+++ app/note-trainer/page.tsx
@@ -102,7 +102,7 @@ export default function Page() {
                   if (chordType === null) return null;
                   return <div key={name}>
                     <label>
-                      C{chordType}&nbsp;
+                      _{chordType}&nbsp;
                       <input type="checkbox" name="chords" value={name} checked={enabledChords[chordType]} onChange={(ev) => onChordEnabledChange(ev, chordType)} />
                     </label>
                   </div>
