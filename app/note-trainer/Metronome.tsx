5114d246dccc42eb2a6623888a76347529368693
Paul Harper <benekastah@gmail.com>
2024-11-07 20:09:34 -0800

    Change settings on first beat

--- app/note-trainer/Metronome.tsx
+++ app/note-trainer/Metronome.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import {Howl, Howler} from 'howler';
+import { Howl } from 'howler';
 import _ from 'lodash';
 
 type Props = {
@@ -34,9 +34,27 @@ export default function Metronome({play, beats, bpm, onHalfBeat}: Props) {
             return { play, bpm, beats };
         }
 
+        function startMetronome() {
+            worker.current?.postMessage({ name: 'START', bpm, beats });
+            setWorkerState(getNextWorkerState());
+        }
+
+        function stopMetronome() {
+            worker.current?.postMessage({ name: 'STOP' });
+            setWorkerState(getNextWorkerState());
+        }
+
         const nextWorkerState = getNextWorkerState();
 
         worker.current.onmessage = (ev) => {
+            if (ev.data === 0) {
+                const nextWorkerState = getNextWorkerState();
+                // If we're on the first beat and bpm or beat have changed, restart the session
+                if (!_.isEqual(workerState, nextWorkerState)) {
+                    startMetronome();
+                    return;
+                }
+            }
             if (ev.data % 2 === 0) {
                 if (ev.data === 0) {
                     clickHi.current.play();
@@ -44,24 +62,17 @@ export default function Metronome({play, beats, bpm, onHalfBeat}: Props) {
                     clickLo.current.play();
                 }
             }
-            if (ev.data === 0) {
-                const nextWorkerState = getNextWorkerState();
-                // If we're on the first beat and bpm or beat have changed, restart the session
-                if (!_.isEqual(workerState, nextWorkerState) {
-                    worker.current?.postMessage({ name: 'START', bpm, beats });
-                    setWorkerState(nextWorkerState);
-                }
-            }
             if (onHalfBeat) onHalfBeat(ev.data);
         };
 
         if (!_.isEqual(workerState, nextWorkerState)) {
             if (play) {
-                worker.current.postMessage({ name: 'START', bpm, beats });
+                if (!workerState.play) {
+                    startMetronome();
+                }
             } else {
-                worker.current.postMessage({ name: 'STOP' });
+                stopMetronome();
             }
-            setWorkerState(nextWorkerState);
         }
     }, [play, bpm, beats, onHalfBeat, workerState, setWorkerState]);
 
